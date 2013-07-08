fb93d08b2038f44c9a73ac53968fcc0e8a7d17a9
Jon Rowe <hello@jonrowe.co.uk>
2013-07-08 13:20:46 +1000

    files extracted from rspec-core

---
+++ lib/rspec/autotest.rb
@@ -0,0 +1,7 @@
+require "rspec/autotest/version"
+
+module Rspec
+  module Autotest
+    # Your code goes here...
+  end
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,24 @@
+require 'rubygems'
+require 'rspec/autorun'
+require 'aruba/api'
+
+if RUBY_PLATFORM == 'java'
+  # Works around https://jira.codehaus.org/browse/JRUBY-5678
+  require 'fileutils'
+  ENV['TMPDIR'] = File.expand_path('../../tmp', __FILE__)
+  FileUtils.mkdir_p(ENV['TMPDIR'])
+end
+
+Dir['./spec/support/**/*.rb'].map {|f| require f}
+
+RSpec.configure do |c|
+
+  c.expect_with :rspec do |expectations|
+    expectations.syntax = :expect
+  end
+
+  # runtime options
+  c.treat_symbols_as_metadata_keys_with_true_values = true
+  c.filter_run :focus
+  c.run_all_when_everything_filtered = true
+end
