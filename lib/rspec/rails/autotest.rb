5b6c297142a686f717a66c0d5f116021b2fc81ee
Abdelkader Boudih <terminale@gmail.com>
2014-05-21 14:02:05 +0000

    remove unused private method and code style

--- lib/rspec/rails/autotest.rb
+++ lib/rspec/rails/autotest.rb
@@ -20,65 +20,67 @@
 # CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-$:.push(*Dir["vendor/rails/*/lib"])
+$:.push(*Dir['vendor/rails/*/lib'])
 
 require 'active_support/core_ext'
 require 'autotest/rspec'
 
-class Autotest::RailsRspec < Autotest::Rspec
+class Autotest
+  class RailsRspec < Autotest::Rspec
 
-  def initialize
-    super
-    setup_rails_rspec_mappings
-  end
-
-  def setup_rails_rspec_mappings
-    %w{coverage/ db/ doc/ log/ public/ script/ tmp/ vendor/rails vendor/plugins vendor/gems}.each do |exception|
-      add_exception(/^([\.\/]*)?#{exception}/)
+    def initialize
+      super
+      setup_rails_rspec_mappings
     end
 
-    clear_mappings
+    def setup_rails_rspec_mappings
+      %w{coverage/ db/ doc/ log/ public/ script/ tmp/ vendor/rails vendor/plugins vendor/gems}.each do |exception|
+        add_exception(/^([\.\/]*)?#{exception}/)
+      end
 
-    add_mapping(%r%^(test|spec)/fixtures/(.*).yml$%) { |_, m|
-      ["spec/models/#{m[2].singularize}_spec.rb"] + files_matching(%r%^spec\/views\/#{m[2]}/.*_spec\.rb$%)
-    }
-    add_mapping(%r%^spec/.*_spec\.rb$%) { |filename, _|
-      filename
-    }
-    add_mapping(%r%^app/models/(.*)\.rb$%) { |_, m|
-      ["spec/models/#{m[1]}_spec.rb"]
-    }
-    add_mapping(%r%^app/views/(.*)$%) { |_, m|
-      files_matching %r%^spec/views/#{m[1]}_spec.rb$%
-    }
-    add_mapping(%r%^app/controllers/(.*)\.rb$%) { |_, m|
-      if m[1] == "application"
-        files_matching %r%^spec/controllers/.*_spec\.rb$%
-      else
-        ["spec/controllers/#{m[1]}_spec.rb"]
-      end
-    }
-    add_mapping(%r%^app/helpers/(.*)_helper\.rb$%) { |_, m|
-      if m[1] == "application" then
-        files_matching(%r%^spec/(views|helpers)/.*_spec\.rb$%)
-      else
-        ["spec/helpers/#{m[1]}_helper_spec.rb"] + files_matching(%r%^spec\/views\/#{m[1]}/.*_spec\.rb$%)
-      end
-    }
-    add_mapping(%r%^config/routes\.rb$%) {
-      files_matching %r%^spec/(controllers|routing|views|helpers)/.*_spec\.rb$%
-    }
-    add_mapping(%r%^config/database\.yml$%) { |_, m|
-      files_matching %r%^spec/models/.*_spec\.rb$%
-    }
-    add_mapping(%r%^(spec/(spec_helper|support/.*)|config/(boot|environment(s/test)?))\.rb$%) {
-      files_matching %r%^spec/(models|controllers|routing|views|helpers)/.*_spec\.rb$%
-    }
-    add_mapping(%r%^lib/(.*)\.rb$%) { |_, m|
-      ["spec/lib/#{m[1]}_spec.rb"]
-    }
-    add_mapping(%r%^app/mailers/(.*)\.rb$%) { |_, m|
-      ["spec/mailers/#{m[1]}_spec.rb"]
-    }
+      clear_mappings
+
+      add_mapping(%r%^(test|spec)/fixtures/(.*).yml$%) { |_, m|
+        ["spec/models/#{m[2].singularize}_spec.rb"] + files_matching(%r%^spec\/views\/#{m[2]}/.*_spec\.rb$%)
+      }
+      add_mapping(%r%^spec/.*_spec\.rb$%) { |filename, _|
+        filename
+      }
+      add_mapping(%r%^app/models/(.*)\.rb$%) { |_, m|
+        ["spec/models/#{m[1]}_spec.rb"]
+      }
+      add_mapping(%r%^app/views/(.*)$%) { |_, m|
+        files_matching %r%^spec/views/#{m[1]}_spec.rb$%
+      }
+      add_mapping(%r%^app/controllers/(.*)\.rb$%) { |_, m|
+        if m[1] == "application"
+          files_matching %r%^spec/controllers/.*_spec\.rb$%
+        else
+          ["spec/controllers/#{m[1]}_spec.rb"]
+        end
+      }
+      add_mapping(%r%^app/helpers/(.*)_helper\.rb$%) { |_, m|
+        if m[1] == "application" then
+          files_matching(%r%^spec/(views|helpers)/.*_spec\.rb$%)
+        else
+          ["spec/helpers/#{m[1]}_helper_spec.rb"] + files_matching(%r%^spec\/views\/#{m[1]}/.*_spec\.rb$%)
+        end
+      }
+      add_mapping(%r%^config/routes\.rb$%) {
+        files_matching %r%^spec/(controllers|routing|views|helpers)/.*_spec\.rb$%
+      }
+      add_mapping(%r%^config/database\.yml$%) { |_, m|
+        files_matching %r%^spec/models/.*_spec\.rb$%
+      }
+      add_mapping(%r%^(spec/(spec_helper|support/.*)|config/(boot|environment(s/test)?))\.rb$%) {
+        files_matching %r%^spec/(models|controllers|routing|views|helpers)/.*_spec\.rb$%
+      }
+      add_mapping(%r%^lib/(.*)\.rb$%) { |_, m|
+        ["spec/lib/#{m[1]}_spec.rb"]
+      }
+      add_mapping(%r%^app/mailers/(.*)\.rb$%) { |_, m|
+        ["spec/mailers/#{m[1]}_spec.rb"]
+      }
+    end
   end
 end
